99fdf673623b08e07785f585ca8466d340275ab2
Vishwanath Hiremath <vishwah1312@gmail.com>
2025-05-06 17:36:54 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0, < 4.0"
+    }
+  }
+}
+
+variable "context" {
+  description = "This variable contains Radius recipe context."
+  type = any
+}
+
+data "azurerm_resource_group" "radius_provided-rg" {
+  name = var.context.azure.resourceGroup
+}
+
+resource "random_password" "password" {
+  length           = 16
+}
+
+resource "azurerm_postgresql_server" "todolist-db" {
+  name                = "todolist-db"
+  location            = data.azurerm_resource_group.radius_provided-rg.location
+  resource_group_name = data.azurerm_resource_group.radius_provided-rg.name
+
+  administrator_login          = "postgres"
+  administrator_login_password = random_password.password.result
+
+  sku_name   = "GP_Gen5_4"
+  version    = "11"
+
+  ssl_enforcement_enabled          = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
+}
+
+output "result" {
+  value = {
+    values = {
+      host = azurerm_postgresql_server.todolist-db.fqdn
+      port = "5432"
+      database = "postgres_db"
+      username = "postgres"
+      password = random_password.password.result
+    }
+  }
+}
